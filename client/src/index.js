bdfd4eea02df1ca17262915a4473e94ee72942c8
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-10-02 16:09:47 -0700

    src index syntax error

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// The ...props means, spread all of the passed props onto this element
-// That way we don't have to define them all individually
-function DeleteBtn(props) {
-  return (
-    <span {...props} role="button" tabIndex="0">
-      ✗
-    </span>
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-export default DeleteBtn;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
