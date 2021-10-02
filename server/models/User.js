9abda12f7098448618a8bedba46054d336660e91
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-10-02 11:45:39 -0700

    added stripe dependency

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const { Schema } = require('mongoose');
+const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
 
 const Order = require('./Order');
