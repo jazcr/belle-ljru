13079f0142d39f53a9b0388ae1432fb45deef2bf
Loryn <lorynhaykus@yahoo.com>
2021-10-03 18:00:00 -0600

    adding image to assets folder

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -9,7 +9,7 @@ import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import './style.css';
 
-const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+const stripePromise = loadStripe('pk_test_51JgHG1K26w07WHByfAHM2hrxkw41soOIIhAabUL4v1k2ZQyOhkjHdNReFaWnH0Us5r27zoqJYptMLsrickPCesBG00OZia2Ixq');
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
