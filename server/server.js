98b98373338c14cc437a3d0574b89fd81332a6b6
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-10-09 12:44:54 -0600

    testing heroku connection

--- server/server.js
+++ server/server.js
@@ -1,28 +1,29 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
 const { typeDefs, resolvers } = require('./schemas');
+// Import `authMiddleware()` function to be configured with the Apollo Server
 const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
-
+const app = express();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
+  // Add context to our server so data from the `authMiddleware()` function can pass data to our resolver functions
+  context: authMiddleware,
 });
 
-(async function () {
-  await server.start();
-  server.applyMiddleware({ app });
-  })();
-  
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Serve up static assets
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
@@ -32,10 +33,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
 
-
 db.once('open', () => {
   app.listen(PORT, () => {
-      console.log(` 🌍 Now listening on localhost:${PORT}`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
