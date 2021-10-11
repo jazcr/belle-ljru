9b39859e4bdaececfb9b0e401eb51d2bbf0149b6
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-10-11 06:55:34 -0600

    centering category menu

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -11,6 +11,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import {
   Typography,
   Link,
+  Button,
   IconButton
 } from '@material-ui/core';
 
@@ -22,9 +23,20 @@ const useStyles = makeStyles({
       fontFamily: "'Ephesis', cursive"
   },
   btn: {
-    paddingBottom: '2%',
-    fontSize: '1.25rem',
-    fontFamily: "'Times New Roman' !important"    
+    // border: "2px solid grey",
+    // borderRadius: "5px",
+    // textDecoration: "underline",
+    // margin: '2px',
+    fontSize: '1rem',
+    fontFamily: "Helvetica",
+
+  },
+  categories: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontFamily: "Helvetica",
+    flexWrap: "wrap",
   }
 })
 
@@ -66,9 +78,9 @@ function CategoryMenu() {
 
   return (
     <>
-      <Typography className={classes.heading} variant='h3'>Belle L'JaRu Skincare: Love the skin you are in!</Typography>
-      {categories.map((item) => (
-        <IconButton className={classes.btn} style={{fontFamily: "'Times New Roman' !important" }}
+<Typography className={classes.heading} variant='h3'>Belle L'JaRu Skincare: Love the skin you are in!</Typography>
+      <div className={classes.categories}>{categories.map((item) => (
+        <IconButton className={classes.btn}
           key={item._id}
           onClick={() => {
             handleClick(item._id);
@@ -82,6 +94,7 @@ function CategoryMenu() {
             All
           </Link>
         </IconButton>
+        </div>
     </>
   );
 }
